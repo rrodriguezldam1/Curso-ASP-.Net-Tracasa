665ff38785845173e5a79606acdde14c92de25b4
Ramon <rrodriguezldam1@gmail.com>
2021-08-13 10:08:14 +0200

    POO

--- 02_POO/Ejemplo03_Interfaces/Program.cs
+++ 02_POO/Ejemplo03_Interfaces/Program.cs
@@ -2,7 +2,7 @@
 using Ejemplo02_Herencia;
 using Ejercicio02_Herencia;
 using System;
-
+using System.Collections.Generic;
 
 namespace Ejemplo03_Interfaces
 {
@@ -34,7 +34,54 @@ namespace Ejemplo03_Interfaces
             Console.WriteLine(fiatNombrable.Nombre);
             Console.WriteLine(fiatENombrable.Nombre);
 
-            
+            Program.EjemploLista();
+        }
+
+        static void EjemploLista() {
+            List<string> textos = new List<string>();
+            // Por defecto, se crean 10 o 20 elementos, pero esto nos da igual
+            // porque lo hace de manera interna
+            textos.Add("1");
+            textos.Add("2");
+            textos.Add("3");
+            textos.Add("4");
+            textos.Add("5");
+            textos.Add("6"); // internamente va usando el array
+            for (int i = 7; i < 30; i++) {
+                textos.Add(i.ToString());
+            }
+            // Llegará un punto que internamente tenga que ampliar dinámicamente
+            textos.RemoveAt(2);
+            textos.Remove("1");
+            for (int i = 0; i < textos.Count; i++)
+            {
+                Console.WriteLine("Lista: " + textos[i]);
+            }
+
+            List<Coche> coches = new List<Coche>();
+            coches.Add(new Coche("Toyota", "Cupra", 50000));
+            coches.Add(new Coche("Tesla", "F21", 40000));
+            IList<Usuario> listaUsu = new List<Usuario>();
+            listaUsu.Add(new Usuario("Usuario listaUsu 1", 0, 0));
+            listaUsu.Add(new Usuario("Usuario listaUsu 2", 0, 0));
+
+            IList<Usuario> arrayUsu = new Usuario[10];
+            arrayUsu[0] = new Usuario("Usuario arrayUsu 2", 0, 0);
+            arrayUsu[1] = new Usuario("Usuario arrayUsu 2", 0, 0);
+            arrayUsu[4] = new Usuario("Usuario arrayUsu 2", 0, 0);
+
+            MostrarColeccion(listaUsu);
+            MostrarColeccion(arrayUsu);
+        }
+
+        static void MostrarColeccion(ICollection<Usuario> coleccion)
+        {
+            Console.WriteLine("Colección usuarios " + coleccion.GetType());
+            foreach (Usuario usu in coleccion)
+            {
+                if (usu != null) //continue;
+                    usu.MostrarDatos();
+            }
         }
     }
 }
---
+++ 02_POO/Ejemplo04_Excepciones/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Ejemplo04_Excepciones
+{
+    class Program
+    {
+        static void FuncionRecursivaInfinita(int i) {
+            if (i < 100) FuncionRecursivaInfinita(50);
+        }
+        static void Main(string[] args)
+        {
+            //Excepciones típicas existentes en .Net (y en otros lenguajes)
+            //StackOverFlow
+            try
+            {
+                // No queda otra que evitarlo
+                FuncionRecursivaInfinita(500);
+            }
+            catch (Exception ex) 
+            {
+                Console.Error.WriteLine("Error");
+            }
+            
+            // CAPTURAR ERROR DE MANERA ESPECÍFICA
+            try
+            {
+                Console.WriteLine("Convirtiendo lo inconvertible!");
+                int.Parse("ramon");
+                Console.WriteLine("Esta línea ni se muestra!");
+            }
+            catch (FormatException ex) //Cuando ocurre una excepcion en control pasa a los bloques catch
+            {
+                Console.Error.WriteLine("Error!! ", ex.Message);
+            }
+
+            // CAPTURAR ERROR DE MANERA GENÉRICA
+            try
+            {
+                Console.WriteLine("Convirtiendo lo inconvertible!");
+                int.Parse("ramon");
+                Console.WriteLine("Esta línea ni se muestra!");
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Error!! ", ex.Message);
+            }
+
+            // CAPTURAR ERROR
+            try
+            {
+                Console.WriteLine("Crear un objeto nulo y tratar de usarlo");
+                object obj = null;
+                Console.WriteLine(obj.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Error!! ", ex.Message);
+            }
+
+            // NO CAPTURAR OTRO ERROR
+            try
+            {
+                Console.WriteLine("Crear un objeto nulo y tratar de usarlo");
+                object obj = null;
+                Console.WriteLine(obj.ToString());
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.Error.WriteLine("Error!! ", ex.Message);
+            }
+
+
+            try
+            {
+                FuncionQueDelegaExcepcion();
+                // La excepción se propaga siempre hacia arriba hasta
+                // que un try catch adecuado la controle
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.Error.WriteLine("Error!! ", ex.Message);
+            }
+
+
+        }
+
+        public static void FuncionQueDelegaExcepcion()
+        {
+            // Si una función lanza una excepción, y no la controlamos aquí
+            // podrá o deberá ser controlada en la función que invoca a esta.
+            int.Parse("ERROR");
+        }
+    }
+}
