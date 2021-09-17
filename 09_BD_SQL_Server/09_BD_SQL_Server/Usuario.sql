685696332e3e002d4218d03f21d2c02f7613f175
Ramon <rrodriguezldam1@gmail.com>
2021-09-17 13:59:57 +0200

    Nuevo proyecto SQL.

---
+++ 09_BD_SQL_Server/09_BD_SQL_Server/Usuario.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Usuario]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [nombre] VARCHAR(MAX) NOT NULL, 
+    [edad] TINYINT NOT NULL, 
+    [altura] FLOAT NOT NULL, 
+    [activo] BIT NOT NULL
+)
